5c54399c46ca8260fac67f133a75031e94be829b
simran <simranag951@gmail.com>
2024-03-24 10:08:03 +0530

    changes forward frontend

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,17 +4,43 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 //import { path } from 'path';
 
 const AppRoutes = () => {
   return (
-    <Routes>
-        <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
-        <Route element={<ProtectedRoute />}> 
-        <Route path="/auth-callback" element={<AuthCallbackPage />}/>
-        </Route>
-        <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
-        <Route path="*" element={<Navigate to="/" />} />
+     <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+     
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
