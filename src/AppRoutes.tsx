b55a365f7847bab5e4870955a8ea09d09c08e172
Abhijeetsabale12 <111568849+Abhijeetsabale12@users.noreply.github.com>
2024-04-01 10:16:27 +0530

    from laptop

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 //import { path } from 'path';
 
 const AppRoutes = () => {
@@ -25,6 +26,14 @@ const AppRoutes = () => {
       <Route path="/detail/:restaurantId" element={<Layout showHero={false}><DetailPage /></Layout>}/>
      
       <Route element={<ProtectedRoute />}>
+      <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
         <Route
           path="/user-profile"
           element={
